b2e8f570d0f70594fd71ae483b06eab0cc601502
Mahdi <akmmahdi115555@gmail.com>
2022-01-28 22:11:58 +0600

    first

---
+++ fn.js
@@ -0,0 +1,150 @@
+
+// Write a JavaScript Function to calculate the multiplication and division of two numbers.
+
+// function numbers(x,y){
+//     multiplication=x*y;
+//     console.log(`The multiplication of ${x} and ${y} is ${multiplication}`);
+//     division=x/y;
+//     console.log(`The division of ${x} and ${y} is ${division}`)
+// }
+// numbers(3,3);
+
+///////////////////////////////////////////////////////////////////
+
+// Write a JavaScript program to convert temperatures from Celsius to Fahrenheit or Vice Versa (Fahrenheit to Celsius).
+
+// function toCelcius(f){
+//     celsius= (5*(f-32))/9;
+//     console.log("celsius values is "+ celsius);
+// }
+// function toFarenheit(c){
+//     farenheit=(9*c+(32*5))/5
+//     console.log("farenhiet values is "+farenheit);
+// }
+// toFarenheit(20);
+// toCelcius(68);
+
+
+// Write a JavaScript function to check from two given integers, whether one is positive and another one is negative. 
+
+// let x=10;
+// let y=13;
+
+// if (x>0 && y<0){
+//     console.log("condition true")
+// }
+// else{
+//     console.log("condition false")
+// }
+
+
+
+// Write a JavaScript function to check whether the number is positive or negative.
+
+// let x=-200;
+
+// if (x>0){
+//     console.log("number is positive")
+// }
+// else{
+//     console.log("number is negative")
+// }
+
+
+
+// Write a JavaScript program to remove a character at the specified position of a given string and return a new string
+
+// let text="fast program"
+// let result=text.replace("fast","second");
+// console.log(result);
+
+// Write a JavaScript function to reverse a given string.
+
+// let text="hsedalgnaB evol ew";
+// function reverse(){
+//    let split= text.split("");
+//    let reverse=split.reverse();
+//    let join=reverse.join("");
+//    console.log(join);
+// }
+
+// reverse();
+
+
+// Create Three objects with at least 4 properties and 2 methods in them. 
+
+// const person={
+//     firstName: "A K M",
+//     lastName: "Mahdi",
+//     age: 25,
+//     height: "5'5",
+
+//     fullName: function(){
+//         return `${this.firstName} ${this.lastName}`
+//     },
+//     details: function(){
+//         return ` age = ${this.age} and height =${this.height}`
+//     }
+// }
+// console.log(person.fullName()+person.details());
+
+
+
+
+// swap the values between two variables
+
+// var x=2;
+// var y=3;
+
+// function swap(){
+//     var temp=x;
+//     x=y;
+//     y=temp;
+//     console.log(`the value of x = ${x} and y = ${y}`)
+// }
+// swap();
+
+
+// Find the largest of 3 numbers
+
+// let x=20, y=32, z=40;
+
+// if (x>y){
+//     console.log("x is the largest")
+// }
+// else if(y>z){
+//     console.log("y is the largest")
+// }
+// else{
+//     console.log("z is the largest")
+// }
+
+// sum of all Numbers in array 
+
+// var numbers =[2,2,3,4,45,5]
+// var count = 0;
+// for(i=0;i<numbers.length;i++){
+//     // console.log(numbers[i]);
+//     count=count+numbers[i];
+// }
+
+// console.log(count);
+
+
+
+// find the largest element in an array
+
+// var numbers =[2,2,113,4,45,5];
+// var largest=numbers[0];
+
+// for(i=0;i<numbers.length;i++){
+//     if(numbers[i]>largest){
+//         largest=numbers[i];
+//     }
+
+// }
+// console.log(largest);
+
+
+// remove duplicate item from an array
+
---
+++ test.js
@@ -0,0 +1,30 @@
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+//replace("Value you want to chnage","value you want to add")    __By default, the replace() method replaces only the first match:
+//trim()  __trim the spaces at the start and end
+
+
+// let str = "Apple, Banana, Kiwi";
+// let part =str.replace("Apple","Mango");
+// console.log(part);
+
+// write a program to find the text "Beautiful" is in the string or not_ Case sensitive doesnt matter 
+
+// let text="Bangladesh is a Beautiful Country"
+// let findTextFrom =text.toUpperCase();
+// let search =findTextFrom.indexOf("BEAUTIFUL");
+
+// if(search==0 || search>=0){
+//     console.log("The text is in the string");
+//     console.log("And the index number is "+search);
+// }
+// else{
+//     console.log("The text is not in the string")
+//     console.log("And the index number is "+search);
+// }
+
+
+// let list ="mango is my favourite fruit";
+// console.log(list[0]);
+
